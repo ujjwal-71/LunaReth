53cc1aa82bc0aa28b3f0a4fa8ea442de69c0d903
ujjwal-71 <ujjwalkumar95655@gmail.com>
2026-06-30 09:59:45 +0530

    modified:   Assets/Settings/Scenes/Player/Attack.cs 	modified:   Assets/Settings/Scenes/Player/Enemy_AI.cs

--- Assets/Settings/Scenes/Player/Attack.cs
+++ Assets/Settings/Scenes/Player/Attack.cs
@@ -5,19 +5,16 @@ public class Attack : MonoBehaviour
     public int damage = 20;
     public Collider2D enemy;
 
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-    }
-
     void OnTriggerEnter2D(Collider2D enemy)
     {
         Enemy_AI enemyAI = enemy.GetComponent<Enemy_AI>();
         if(enemyAI != null)
         enemy.GetComponent<Enemy_AI>().Damaging(20);
+    }
+    void OnTriggerExit2D(Collider2D enemy)
+    {
+        Enemy_AI enemyAI = enemy.GetComponent<Enemy_AI>();
+        if(enemyAI != null)
+        enemy.GetComponent<Enemy_AI>()._Damaged();
     }
 }
--- Assets/Settings/Scenes/Player/Enemy_AI.cs
+++ Assets/Settings/Scenes/Player/Enemy_AI.cs
@@ -1,20 +1,28 @@
+using System;
 using UnityEngine;
 
 public class Enemy_AI : MonoBehaviour
 {
     public Collider2D Attack_Box;
+    private SpriteRenderer sprite_enemy;
     public int Max_Health = 100;
     int Current_Health = 100;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Current_Health = Max_Health;
+        sprite_enemy = GetComponent<SpriteRenderer>();
     }
     public void Damaging(int Damage)
     {
         Current_Health -= Damage;
+        sprite_enemy.color = new Color(1,0.2f,0.3f,1);
         if(Current_Health<=0)
             Die();
+    }
+    public void _Damaged()
+    {
+        sprite_enemy.color = new Color(1,1,1,1);
     }
     void Die()
     {
