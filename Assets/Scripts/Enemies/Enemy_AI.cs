d33c7c32e50663d7dd9d15e10a0fcc3550fc89ba
ujjwal-71 <ujjwalkumar95655@gmail.com>
2026-08-07 12:23:54 +0530

    modified:   Assets/Scenes/SampleScene.unity 	modified:   Assets/Scripts/Enemies/Enemy_AI.cs

--- Assets/Scripts/Enemies/Enemy_AI.cs
+++ Assets/Scripts/Enemies/Enemy_AI.cs
@@ -1,5 +1,6 @@
-using Unity.VisualScripting;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
 public class Enemy_AI : MonoBehaviour
 {
@@ -8,12 +9,13 @@ public class Enemy_AI : MonoBehaviour
     private SpriteRenderer sprite_enemy;
     public Transform PlayerTransform;
     private Rigidbody2D EnemyRB;
+    public GameObject waypointsGameObj;
     private Transform[] wayPoints;
+    public int speed = 30;
 
     [Header("Stats")]
     public int Max_Health = 100;
     private int Current_Health = 100;
-    private Transform nextPoint;
 
     private enum enemyState
     {
@@ -27,20 +29,22 @@ public class Enemy_AI : MonoBehaviour
 
     private void Awake()
     {
-        wayPoints = new Transform[transform.childCount];
-        for (int i=0; i<transform.childCount; i++)
+        wayPoints = new Transform[waypointsGameObj.transform.childCount];
+        for (int i=0; i<waypointsGameObj.transform.childCount; i++)
         {
-            wayPoints[i] = transform.GetChild(i);
+            wayPoints[i] = waypointsGameObj.transform.GetChild(i);
         }
-        nextPoint = wayPoints[0];
         currentState = enemyState.Patrol;
         Current_Health = Max_Health;
         sprite_enemy = GetComponent<SpriteRenderer>();
         EnemyRB = GetComponent<Rigidbody2D>();
     }
 
+    private float playerDistance;
     void Update()
     {
+        currentState = enemyState.Chasing;
+        playerDistance = PlayerTransform.position.x - transform.position.x;
         switch (currentState)
         {
             case enemyState.Patrol:
@@ -60,16 +64,45 @@ public class Enemy_AI : MonoBehaviour
         }
     }
 
+    private int wayPointsIndex = 0;
+    private float waitTimer = 0;
+    private float distance;
     private void Patrol()
     {
-        if (nextPoint == wayPoints[transform.childCount-1])
-            nextPoint = wayPoints[0];
-        transform.Translate(1f,0f,0f,nextPoint);
+        distance = wayPoints[wayPointsIndex].position.x - transform.position.x;
+        
+        if (distance < 0.2 && distance > -0.2)
+        {
+            if (waitTimer < 1f)
+            {
+                EnemyRB.linearVelocityX = 0;
+                waitTimer += Time.deltaTime;
+            }
+            else
+            {
+                waitTimer = 0;
+                if (wayPointsIndex == waypointsGameObj.transform.childCount-1)
+                    wayPointsIndex = 0;
+                else
+                    wayPointsIndex++;
+            }
+        }
+        else
+        {
+            if (Mathf.Sign(distance) == -1)
+                transform.rotation = Quaternion.Euler(0,0,0);
+            else
+                transform.rotation = Quaternion.Euler(0,180,0);
+
+            EnemyRB.linearVelocityX = Mathf.Sign(distance) * speed;
+        }
     }
-    
     private void Chasing()
     {
-        
+        if (playerDistance > 0.2f || playerDistance < -0.2f)
+        {
+            EnemyRB.linearVelocityX = Mathf.Sign(playerDistance) * speed;
+        }
     }
 
     private void Healing()
@@ -90,7 +123,7 @@ public class Enemy_AI : MonoBehaviour
         
         if (Current_Health <= 0)
         {
-            Die();
+            Destroy(gameObject);
         }
     }
 
@@ -99,8 +132,4 @@ public class Enemy_AI : MonoBehaviour
         sprite_enemy.color = new Color(1, 1, 1, 1);
     }
 
-    private void Die()
-    {
-        Destroy(gameObject);
-    }
 }
